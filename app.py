e287c0148dd9caf48f482450e47bb5f240f9aadd
Roxy223 <roxykplays@gmail.com>
2025-12-04 23:20:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,10 @@ def index():
     return render_template("index.html", stops=stops_with_delays)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
